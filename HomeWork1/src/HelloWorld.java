2b19624b510b9536e76ddacd970316f6a648bf8e
TremoziDaniel <daniilizmajlov@gmail.com>
2022-11-03 19:32:55 +0100

    HW-1
    Loaded

---
+++ HomeWork1/src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Garfield", "red", 6);
+        cat.voice();
+        System.out.println(cat);
+    }
+}
