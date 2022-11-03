609cdb3722fc6735f171ca06e2bddbee14f3438d
TremoziDaniel <daniilizmajlov@gmail.com>
2022-11-03 19:48:48 +0100

    Sorted
    Made some mistakes and solved it

--- HomeWork1/src/HelloWorld.java
+++ HomeWork1/src/HelloWorld.java
@@ -1,4 +1,12 @@
+/*
+    * Java Prof. Homework №1
+    *
+    * @author Danil Izmajlov
+    * @version 31.10 ~ 31.10
+ */
+
 public class HelloWorld {
+
     public static void main(String[] args) {
         Cat cat = new Cat("Garfield", "red", 6);
         cat.voice();
